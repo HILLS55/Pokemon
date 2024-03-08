3dd95dc1f46a7866e59c5ba7b339318d7138b90e
HILLS <zyrick@hotmail.fr>
2024-03-08 12:22:50 +0100

    Mise en place du projet, création de nos pages et system de rooting

---
+++ src/Pages/GenerationsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GenerationsPage = () => {
+    // Etats  -> States
+
+  //  Comportements -> Les Functions
+
+  //  Affichage -> return
+  return <>
+    <h1>Générations Page</h1>
+  </>
+}
+
+export default GenerationsPage
---
+++ src/Pages/PokemonsPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PokemonsPage = () => {
+    // Etats  -> States
+
+  //  Comportements -> Les Functions
+
+  //  Affichage -> return
+  return <> 
+    <h1>Pokemons</h1>
+  </>
+}
+
+export default PokemonsPage;
